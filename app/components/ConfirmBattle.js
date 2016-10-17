d25a420f3babfbf7e5fdd863cab829cb508527d7
Simon White <simon.white@proximity.co.nz>
2016-10-17 16:52:59 +1300

    completed more sections of the tutorial

--- app/components/ConfirmBattle.js
+++ app/components/ConfirmBattle.js
@@ -4,16 +4,14 @@ var styles = require('../styles');
 var Link = require('react-router').Link;
 var UserDetails = require('./UserDetails');
 var UserDetailsWrapper = require('./UserDetailsWrapper');
-
-function puke (object) {
-	return <pre>{JSON.stringify(object, null, ' ')}</pre>
-}
+var MainContainer = require('./MainContainer');
+var Loading = require('./Loading');
 
 //functional stateless component
 function ConfirmBattle (props) {
 	return props.isLoading === true
-	? <p> Loading! </p>
-	: <div className="jumbotron col-sm-12 text-center" style={styles.transparentBg}>
+	? <Loading text='One Moment' speed="400"/>
+	: <MainContainer>
 		<h1>Confirm Players</h1>
 		<div className='col-sm-8 col-sm-offset-2'>
 			<UserDetailsWrapper header='Player 1'>
@@ -37,7 +35,7 @@ function ConfirmBattle (props) {
 				</Link>
 			</div>
 		</div>
-	</div>
+	</MainContainer>
 }
 
 ConfirmBattle.propTypes = {
